6a2b8ffd8cca261ee978bb77f2682547c0273e56
r.navabian <r.navabian@NAVABIAN>
2023-01-23 09:34:50 +0330

    Add project files.

---
+++ DecoratorPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DecoratorPattern;
+
+//var write = new Write();
+//write.write();
+//var star =new AddStar();
+//star.write();
+//var dash = new AddDash();
+//dash.write();
+
+IWriter writer = new AddDashDecorator(new AddStarDecorator(new Write()));
+writer.write();
+
+var rl = Console.ReadLine();
+
---
+++ DecoratorPattern/Write.cs
@@ -0,0 +1,69 @@
+﻿namespace DecoratorPattern
+{
+    public interface IWriter
+    {
+        void write();
+    }
+    public class Write : IWriter
+    {
+        public virtual void write()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+    // Inherite Model
+    //public class AddDash : Write
+    //{
+    //    public override void write()
+    //    {
+    //        Console.WriteLine("----------");
+    //        base.write();
+    //        Console.WriteLine("----------");
+    //    }
+    //}
+
+    public class AddDashDecorator : IWriter
+    {
+        private readonly IWriter _writer;
+        public AddDashDecorator(IWriter writer)
+        {
+            _writer = writer;
+        }
+
+        public void write()
+        {
+            Console.WriteLine("----------");
+            _writer.write();
+            Console.WriteLine("----------");
+        }
+    }
+    // Inherite Model
+
+    //public class AddStar : AddDash
+    //{
+    //    public override void write()
+    //    {
+    //        Console.WriteLine("********");
+    //        base.write();
+    //        Console.WriteLine("********");
+    //    }
+    //}
+
+    public class AddStarDecorator : IWriter
+    {
+        private readonly IWriter _writer;
+
+        public AddStarDecorator(IWriter writer)
+        {
+            _writer = writer;
+        }
+
+        public void write()
+        {
+            Console.WriteLine("********");
+            _writer.write();
+            Console.WriteLine("********");
+        }
+    }
+}
